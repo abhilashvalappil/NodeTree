7dda247fe9076ad9e6ecbf47e5d7abe3fe81db7b
abhilashvalappil <abhilashvalappil123@gmail.com>
2026-04-02 14:26:12 +0530

    add constants for HTTP status codes and response messages

---
+++ server/src/constants/messageConstants.ts
@@ -0,0 +1,7 @@
+export const Messages = {
+    FAILED: "Failed to process the request",
+    SERVER_ERROR: "Internel server error",
+    NODE_DELETED: "Node deleted successfully",
+    ID_REQUIRED: "Node id is required",
+    NAME_REQUIRED: "A valid name is required"
+};
